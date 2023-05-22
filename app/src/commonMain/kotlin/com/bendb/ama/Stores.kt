98acbb215641a0a36b773915828f67eef1207f3d
Ben Bader <ben@bendb.com>
2023-05-21 21:53:33 -0600

    missing license headers

--- app/src/commonMain/kotlin/com/bendb/ama/Stores.kt
+++ app/src/commonMain/kotlin/com/bendb/ama/Stores.kt
@@ -1,8 +1,23 @@
+// Amanuensis - a local web proxy for debugging
+// Copyright (C) 2023 Benjamin Bader
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 package com.bendb.ama
 
 import io.github.xxfast.kstore.KStore
 import io.github.xxfast.kstore.file.storeOf
-import net.harawata.appdirs.AppDirs
 import net.harawata.appdirs.AppDirsFactory
 
 fun getConfigurationStorage(): KStore<Configuration> {
